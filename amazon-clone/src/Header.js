4e78587e4a50d6f62ef60241ee8904dcb6967d78
Daniel Armbruster <danielgundam00@gmail.com>
2022-04-17 21:29:48 -0600

    added more styling and text elements to header

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,6 +1,7 @@
 import "./Header.css"
 import { Link } from "react-router-dom"
 import SearchIcon from '@mui/icons-material/Search';
+import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined';
 
 function Header(){
     return(
@@ -8,6 +9,13 @@ function Header(){
             <Link to="/">
                 <img className="header_logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo"/>
             </Link>
+            <span className="header_address">
+            <FmdGoodOutlinedIcon  className="header_adressIcon"/>
+            <span className="header_address_text">
+            <p>Hello</p>
+            <p className="address_strong">Select your address</p>
+            </span>
+            </span>
             <div className="header_search">
                 <select name="options" className="header_selectOption">
                     <option value="All">All</option>
